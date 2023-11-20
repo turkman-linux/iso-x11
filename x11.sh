4e36254324aceadd50f25dd6e397d555db3bc312
sulincix <aliriza.keskin@pardus.org.tr>
2023-11-20 12:58:39 +0000

    Update x11.sh

--- x11.sh
+++ x11.sh
@@ -4,6 +4,7 @@ ymp repo --update --allow-oem --ignore-gpg
 ymp it xinit xorg-server xterm elogind freetype xauth xkbcomp xkeyboard-config @x11.drivers --no-emerge --allow-oem
 sed -i "s/#USER/USER/g" /etc/conf.d/xinit
 rc-update add elogind
+rc-update add devfs
 rc-update add eudev
 rc-update add udhcpc
 rc-update add hostname
